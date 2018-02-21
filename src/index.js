1c602d349efe7d09cf7b71782241bfad0fc4b711
molestador <hustlerxxl@gmail.com>
2018-02-22 00:07:52 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -3,5 +3,15 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
-};
+  let c = 0;
+  for (let i = 0; i < preferences.length; i++) {
+    for (let j = 0; j < preferences.length; j++) {
+      for (let k = 0; k < preferences.length; k++) {
+        if (i === preferences[j] - 1 && j === preferences[k] - 1 && k === preferences[i] - 1) {
+          c++;
+        }
+      }
+    }
+  }
+  return Math.floor(c / 3);
+}
